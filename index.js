951241f4df4eaa11236d01db5a760f1ba6688f47
Kamil Partyka <pkamil14@gmail.com>
2020-04-15 17:11:00 +0200

    completion of js and some improvements

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const quizQuestion = document.querySelector('.quiz__question');
 const timerCouter = document.querySelector('.quiz__timer-couter');
 const timeGauge = document.querySelector('.quiz__timer-gaugeB');
 const quizAnswers = document.querySelector('.quiz__answers');
+const questionCounter = document.querySelector('.quiz__questionCounter');
 
 // variables
 
@@ -38,8 +39,14 @@ const questionsArr = [
 
 let lastQuestionIndex = questionsArr.length - 1;
 let runingQuestionIndex = 0;
-let TIMER;
 
+const timeForAnwser = 15;
+const gaugeWidth = 150;
+let count = timeForAnwser;
+const gaugeProgress = gaugeWidth / timeForAnwser;
+
+let TIMER;
+let score = 0;
 // render question function
 
 function renderQuestion() {
@@ -47,22 +54,18 @@ function renderQuestion() {
 	quizImg.innerHTML = `<img src="https://via.placeholder.com/150" alt="question img" />`;
 	quizQuestion.innerHTML = `<p> ${question.question} </p>`;
 	quizAnswers.innerHTML = `
-					<div class="quiz__answers-choice" id="A">
+					<div class="quiz__answers-choice" onclick="checkAnswer('A')">
 						${question.answerA}
 					</div>
-					<div class="quiz__answers-choice" id="B">
+					<div class="quiz__answers-choice" onclick="checkAnswer('B')">
 						${question.answerB}
 					</div>
-					<div class="quiz__answers-choice" id="C">
+					<div class="quiz__answers-choice" onclick="checkAnswer('C')">
 						${question.answerC}
 					</div>`;
 }
 
 // render counter
-const timeForAnwser = 15;
-const gaugeWidth = 150;
-let count = timeForAnwser;
-const gaugeProgress = gaugeWidth / timeForAnwser;
 
 function renderCounter() {
 	if (count >= 0) {
@@ -76,12 +79,56 @@ function renderCounter() {
 		if (runingQuestionIndex < lastQuestionIndex) {
 			runingQuestionIndex++;
 			renderQuestion();
+			renderQuestionCounter();
 		} else {
 			clearInterval(TIMER);
+			renderScore();
 			quizSection.classList.toggle('hide');
 			scoreSection.classList.toggle('hide');
 		}
 	}
+}
+
+// check answer
+
+function checkAnswer(answer) {
+	if (answer === questionsArr[runingQuestionIndex].correctAnswer) {
+		score++;
+	}
+	count = timeForAnwser;
+	if (runingQuestionIndex < lastQuestionIndex) {
+		runingQuestionIndex++;
+		renderQuestion();
+		renderQuestionCounter();
+	} else {
+		clearInterval(TIMER);
+		renderScore();
+		quizSection.classList.toggle('hide');
+		scoreSection.classList.toggle('hide');
+	}
+}
+
+// render question counter
+
+function renderQuestionCounter() {
+	questionCounter.innerHTML = `<p class="score__text">Pytanie: ${runingQuestionIndex} z ${questionsArr.length}</p>`;
+}
+
+// render Score
+
+function renderScore() {
+	const scorePerCent = Math.round((100 * score) / questionsArr.length);
+	scoreSection.innerHTML = `
+				<img
+					class="score__img"
+					src="${scorePerCent > 50 ? 'img/happy-icon.png' : 'img/sad-icon.png'}"
+					alt="score img"
+				/>
+				<p class="score__text">Poprawne odpowiedzi: ${score} z ${
+		questionsArr.length
+	}</p>
+	<button class="score__restartButton" onclick='restartButton()'>Od nowa</button>
+	`;
 }
 
 // startButton event listener
@@ -90,5 +137,12 @@ startButton.addEventListener('click', () => {
 	startSection.classList.toggle('hide');
 	quizSection.classList.toggle('hide');
 	renderQuestion();
+	renderQuestionCounter();
 	TIMER = setInterval(renderCounter, 1000);
 });
+
+// restartButton function
+
+function restartButton() {
+	location.reload();
+}
